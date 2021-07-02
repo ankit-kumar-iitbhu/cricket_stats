4d6aaaa5b79d705fe535becf941690e0b36deae1
Ankit Kumar <ankit.kumar.btech21@gmail.com>
2021-07-02 22:58:39 +0530

    Action Taken : 1. Added boiler plate code for spring boot application using spring.io.

---
+++ cricket-stats/src/test/java/com/ankit/cricketstats/CricketStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankit.cricketstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricketStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
